be4d1a61fa7422716e1e858e9637f28086e96e70
jason zheng <101104044@nccu.edu.tw>
2018-09-27 23:50:57 +0800

    add components and login component contents

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+
+// @NgModule({
+//   imports: [
+//     CommonModule
+//   ],
+//   declarations: []
+// })
+// export class AppRoutingModule { }
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { DessertComponent } from './dessert/dessert.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'dessert', component: DessertComponent},
+  {path: 'login', component: LoginComponent}
+  // { path: 'activity/:id', component: ViewComponent },
+  // { path: 'page/:id', component: ViewComponent },
+  // { path: 'view', component: ViewComponent },
+  // {path: '', redirectTo: '/9487', pathMatch: 'full'},
+  // {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: MainComponent }
+];
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AccordionModule} from 'primeng/accordion';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { MainComponent } from './main/main.component';
+import { DessertComponent } from './dessert/dessert.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent,
+    DessertComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AccordionModule
+    AccordionModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
