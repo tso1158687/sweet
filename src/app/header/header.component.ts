426a0060f593b05116f30a4b86edaead7656a6c6
jason zheng <101104044@nccu.edu.tw>
2018-10-04 00:41:33 +0800

    modified header and used bs4 nav bar without jquery

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  navbarOpen = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
