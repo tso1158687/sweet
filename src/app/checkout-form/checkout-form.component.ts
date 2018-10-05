0235ec2170e866afa523a41b3474b8e7f80e59fe
jason <101104044@nccu.edu.tw>
2018-10-05 12:40:24 +0800

    預備判斷邏輯

--- src/app/checkout-form/checkout-form.component.ts
+++ src/app/checkout-form/checkout-form.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutFormComponent implements OnInit {
   step = 1;
+  // 運送、付款、發票
+  title = '運送'
   constructor() { }
 
   ngOnInit() {
   }
   nextStep() {
     this.step += 1;
+    switch (this.step) {
+      case 1:
+        this.title = "運送"
+        break;
+      case 2:
+        this.title = "付款"
+        break;
+      case 3:
+        this.title = "發票"
+        break;
+    }
   }
 
 }
